05b6ba664da4b8489775565ef8d81532dfd1a760
obadajasm <obada454u2u@gmail.com>
2020-05-24 17:04:17 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/dio_cache/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dio_cache
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter cache with dio',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DioCacheManager _dioCacheManager;
+  String _myData;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          FlatButton(
+            child: Text(
+              'getData',
+            ),
+            onPressed: () async {
+              _dioCacheManager = DioCacheManager(CacheConfig());
+
+              Options _cacheOptions = buildCacheOptions(Duration(days: 7));
+              Dio _dio = Dio();
+              _dio.interceptors.add(_dioCacheManager.interceptor);
+              Response response = await _dio.get(
+                  'https://jsonplaceholder.typicode.com/users',
+                  options: _cacheOptions);
+              setState(() {
+                _myData = response.data.toString();
+              });
+              print(_myData);
+            },
+          ),
+          FlatButton(
+            child: Text(
+              'Delete Cache',
+            ),
+            onPressed: () async {
+              if (_dioCacheManager != null) {
+                bool res = await _dioCacheManager.deleteByPrimaryKey(
+                    'https://jsonplaceholder.typicode.com/users');
+                print(res);
+              }
+            },
+          ),
+          Text(
+            _myData ?? '',
+            // style: Theme.of(context).textTheme.headline1,
+          ),
+        ],
+      ),
+    );
+  }
+}
